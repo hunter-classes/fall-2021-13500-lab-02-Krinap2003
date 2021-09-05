f5806057e32152a2b8b023a529bc31d385926e61
krina <krinap@DESKTOP-DLKQ5RN.localdomain>
2021-09-05 16:32:43 -0400

    add valid.cpp

--- valid.cpp
+++ valid.cpp
@@ -9,14 +9,17 @@ Assignment: Lab 2 part A
 
 int main()
 {
+  //Enter an integer from 1 to 100
   int num;
   std::cout<<"Please enter an integer: ";
   std::cin>>num;
+  //if the integer is less than 0 or more than 100 enter the integer again
   while(num < 0 || num > 100)
     {
       std::cout<<"Please re-enter: ";  
       std::cin>>num;
     }
+  //print the a squared integer of the entered integer
    num = num*num;                                                                                        
    std::cout<<"Number squared is: "<<num<<std::endl; 
     
